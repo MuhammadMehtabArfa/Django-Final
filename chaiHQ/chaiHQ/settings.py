99cfa2489b580ec5bdd5e037fc080193c93358fc
MUHAMMADMEHTABARFA <muhammadmehtabarfa@example.com>
2024-10-28 11:38:52 -0700

    Did models views and urls

--- chaiHQ/chaiHQ/settings.py
+++ chaiHQ/chaiHQ/settings.py
@@ -133,6 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # media configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
